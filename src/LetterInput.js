1275dbcc2eb0ff2951033d3f6dbd916d95bfd820
H-Q7 <hussam.a.qannam@gmail.com>
2023-05-23 08:54:16 +0300

    refactor

---
+++ src/LetterInput.js
@@ -0,0 +1,177 @@
+export class LetterInput extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+        <style> 
+        input {
+          text-transform: uppercase;
+          width: 95%;
+          aspect-ratio: 0.5;
+          border-radius: 10%;
+          background-color: rgb(37, 37, 37);
+          border: none;
+          box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+          color: white;
+          text-align: center;
+          padding: 0;
+          transiton: 0.2s;
+        }
+        input:focus-within {
+          outline: none;
+          border: 1px solid gray; 
+        }
+
+        section {
+          position: relative;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+        }
+
+        div {
+          background: rgb(82, 128, 82);
+          width: 5px;
+          height: 10px;
+          border-radius: 10px;
+          opacity: 0;
+          position: absolute;
+        }
+        .right {
+          right: 0;
+        }
+
+        .left {
+          left: 0;
+        }
+
+        .up {
+          animation: moveu-Ani 0.25s;
+        }
+
+        .down {
+          animation: moved-Ani 0.25s;
+        }
+
+        @keyframes moveu-Ani {
+          1% {
+            opacity: 1;
+          }
+          100% {
+            opacity 0;
+            transform: translateY(-20px);
+          }
+        }
+
+        @keyframes moved-Ani {
+          1% {
+            opacity: 1;
+          }
+          100% {
+            opacity 0;
+            transform: translateY(10px);
+          }
+        }
+
+        .shake {
+          animation: shakeMove 0.5s;
+        }
+
+        @keyframes shakeMove {
+          1% {
+          }
+          50% {
+            transform: translateY(5px);
+          }
+
+          100% {
+            transform: translateY(0);
+          }
+        }
+
+        .rotate {
+          animation: rotateMove 0.5s;
+        }
+
+        @keyframes rotateMove {
+          1% {
+          }
+          20% {
+            transform: rotate(25deg);
+          }
+          40% {
+            transform: rotate(-25deg);
+          }
+          60% {
+            transform: rotate(25deg);
+          }
+          80 {
+            transform: rotate(-25deg);
+          }
+
+          100% {
+            transform: rotate(0);
+          }
+        }
+
+        </style>
+
+
+        <section class="upper">
+        <div class="left"></div><div></div><div class="right"></div>
+        </section>
+        <input type="text" maxlength="1"> 
+        </input>
+        <section class="lower">
+        <div class="left"></div><div></div><div class="right"></div>
+        </section>`;
+  }
+
+  changeState(state) {
+    const particles = this.shadowRoot.querySelectorAll("div");
+    const letterBox = this.shadowRoot.querySelector("input");
+    if (state == "semiCorrect") {
+      document.querySelector(".correctLetters").textContent = document.querySelector(".correctLetters").textContent + letterBox.value.toUpperCase();
+      letterBox.style = "background: hsl(49, 31%, 29%);";
+      letterBox.value = '';
+    } else if ((state == "correct")) {
+      document.querySelector(".correctLetters").textContent = document.querySelector(".correctLetters").textContent + letterBox.value.toUpperCase();
+      letterBox.disabled = "true";
+      letterBox.style = "background: hsl(120, 22%, 21%);";
+    } else {
+      letterBox.classList.toggle("rotate");
+      setTimeout(() => {
+        letterBox.classList.toggle("rotate");
+      }, 1000);
+      letterBox.value = '';
+      return
+    }
+
+    console.log(particles);
+
+    particles.forEach(particle => {
+      if (state == "semiCorrect") {
+        particle.style = "background: rgb(163, 149, 86);";
+      } else if ((state = "correct")) {
+        particle.style = "background: rgb(82, 128, 82);";
+      }
+    });
+    this._toggleAnimation(particles, letterBox);
+    setTimeout(() => {
+      this._toggleAnimation(particles, letterBox);
+    }, 1000);
+  }
+
+  _toggleAnimation(particles, letterBox) {
+    particles.forEach(particle => {
+      if (particle.parentElement.classList.contains("upper")) {
+        particle.classList.toggle("up");
+      } else {
+        particle.classList.toggle("down");
+      }
+    });
+    letterBox.classList.toggle("shake");
+  }
+}
+
+customElements.define("hq7-letter", LetterInput);
--- src/app.js
+++ src/app.js
@@ -1,34 +1,64 @@
-const api = "f4f38cf257144c4798846f1ae1884446";
+import { LetterInput } from "./LetterInput.js";
 
-const RandomGamePicker = async () => {
+const api = "f4f38cf257144c4798846f1ae1884446";
+let game;
+
+const randomGamePicker = async () => {
   const randomPageNum = Math.floor(Math.random() * 10) + 1;
   const randomGameNum = Math.floor(Math.random() * 20);
-  console.log(randomPageNum, randomGameNum);
-  const game = await fetch(
+  game = await fetch(
     `https://api.rawg.io/api/games?key=${api}&dates=2015-01-01,2023-01-01&ordering=-added&page=${randomPageNum}`
   )
     .then(result => result.json())
-    .then(result => result.results[randomGameNum]);
-    console.log(game);
-    if (game.name.length > 15) {
-      return
-    }
-  imgRenderer(game.background_image);
-  createInput(game.name);
+    .then(result => result.results[randomGameNum])
+    .catch(error => {});
+  console.log(game);
+
+  imgRenderer();
+  createInput();
 };
 
-const imgRenderer = gameImg => {
+const imgRenderer = () => {
   const imgElement = document.querySelector("img");
-  imgElement.src = gameImg;
+  imgElement.src = game.background_image;
 };
 
-const createInput = gameName => {
-  const textField = document.querySelector(".textField");
-  for (let i = 0; i < gameName.length; i++) {
-    const textElement = document.createElement("input");
-    textElement.setAttribute("type", "text");
-    textField.appendChild(textElement);
+const getLetterSize = () => {
+  let LargestWordLength = 0;
+  let numOfRow = 0;
+  for (let i = 0; i < game.slug.length; i++) {
+    let j = i;
+    for (; j < game.slug.length; j++) {
+      if (game.slug[j] == "-" || game.slug[j] == "_") {
+        break;
+      }
+    }
+    numOfRow++;
+    j = j - i;
+    LargestWordLength = j > LargestWordLength ? j : LargestWordLength;
+    i = i + j;
   }
+  return LargestWordLength + numOfRow;
 };
 
-RandomGamePicker();
+const createInput = () => {
+  const textField = document.querySelector(".textField");
+  const sizeOfElement =
+    textField.getClientRects()[0].width / getLetterSize();
+  let row = document.createElement("div");
+  row.className = "row";
+  for (let i = 0; i < game.slug.length; i++) {
+    if (game.slug[i] == "-" || game.slug[i] == "_") {
+      textField.appendChild(row);
+      row = document.createElement("div");
+      row.className = "row";
+      continue;
+    }
+    const newLetter = document.createElement("hq7-letter");
+    newLetter.style = `width: min(${sizeOfElement}px,30px)`;
+    row.appendChild(newLetter);
+  }
+  textField.appendChild(row);
+};
+
+randomGamePicker();
